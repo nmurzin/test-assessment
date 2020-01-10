efb20f5bbd5cd95d9c4ad4ca14bb2de29e179d9d
Nick Murzin <r0s0m4xa@gmail.com>
2020-01-10 14:10:19 +0200

    Refactor test

--- tests/TotalPriceCalculationTest.php
+++ tests/TotalPriceCalculationTest.php
@@ -21,56 +21,50 @@ class TotalPriceCalculationTest extends TestCase
     private Terminal $terminal;
 
     /**
-     * TotalPriceCalculationTest constructor.
+     * TotalPriceCalculationTest set up.
      */
-    public function __construct()
+    protected function setUp(): void
     {
+        $prices = [
+            'ZA' => [1 => 2.00, 4 => 7.00],
+            'YB' => [1 => 12.00],
+            'FC' => [1 => 1.25, 6 => 6.00],
+            'GD' => [1 => 0.15],
+        ];
+
         $this->terminal = new ShopTerminal(new Cart(), new PriceList());
 
-        $this->terminal->setPricing(new PriceEntity('ZA', [1 => 2.00, 4 => 7.00]));
-        $this->terminal->setPricing(new PriceEntity('YB', [1 => 12.00]));
-        $this->terminal->setPricing(new PriceEntity('FC', [1 => 1.25, 6 => 6.00]));
-        $this->terminal->setPricing(new PriceEntity('GD', [1 => 0.15]));
-
-        parent::__construct();
+        foreach ($prices as $productKey => $productPrices) {
+            $this->terminal->setPricing(new PriceEntity($productKey, $productPrices));
+        }
     }
 
-	public function testFirstDataSet()
-	{
-		$dataSet = ['ZA', 'YB', 'FC', 'GD', 'ZA', 'YB', 'ZA', 'ZA'];
-		$expectedResult = 32.40;
-		$this->terminal->clearCart();
+    /**
+     * @param  array  $productList
+     * @param  float  $expectedPrice
+     *
+     * @dataProvider productsDataProvider
+     */
+    public function testTotalPriceCalculation(array $productList, float $expectedPrice): void
+    {
+        $this->terminal->clearCart();
 
-		foreach ($dataSet as $item) {
-			$this->terminal->scanItem($item);
-		}
+        foreach ($productList as $productKey) {
+            $this->terminal->scanItem($productKey);
+        }
 
-		$this->assertEquals($expectedResult, $this->terminal->getTotal());
-	}
+        $this->assertEquals($expectedPrice, $this->terminal->getTotal());
+    }
 
-	public function testSecondDataSet()
-	{
-		$dataSet = ['FC', 'FC', 'FC', 'FC', 'FC', 'FC', 'FC'];
-		$expectedResult = 7.25;
-		$this->terminal->clearCart();
-
-		foreach ($dataSet as $item) {
-			$this->terminal->scanItem($item);
-		}
-
-		$this->assertEquals($expectedResult, $this->terminal->getTotal());
-	}
-
-	public function testThirdDataSet()
-	{
-		$dataSet = ['ZA', 'YB', 'FC', 'GD'];
-		$expectedResult = 15.40;
-		$this->terminal->clearCart();
-
-		foreach ($dataSet as $item) {
-			$this->terminal->scanItem($item);
-		}
-
-		$this->assertEquals($expectedResult, $this->terminal->getTotal());
-	}
+    /**
+     * @return array
+     */
+    public function productsDataProvider(): array
+    {
+        return [
+            [['ZA', 'YB', 'FC', 'GD', 'ZA', 'YB', 'ZA', 'ZA'], 32.40],
+            [['FC', 'FC', 'FC', 'FC', 'FC', 'FC', 'FC'], 7.25],
+            [['ZA', 'YB', 'FC', 'GD'], 15.40]
+        ];
+    }
 }
